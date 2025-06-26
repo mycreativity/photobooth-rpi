0952ee4a645a01c1e95aac86c0bc208c86a5549d
Jasper Steenweg <jaspersteenweg@Jaspers-MacBook-Pro.local>
2025-06-26 20:43:52 +0200

    working 2D version

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame
 pygame.init()
 import config
-import moderngl
-from OpenGL.GL import glEnable, GL_MULTISAMPLE
 from screens.preview_screen import PreviewScreen
 from screens.screen_manager import ScreenManager
 from screens.start_screen import StartScreen
@@ -10,18 +8,9 @@ from ui.fps_counter import FPSCounter
 
 # 1. Init pygame with OpenGL settings
 pygame.display.set_caption("Photobooth")
-pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
-pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
-pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
-pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLEBUFFERS, 1)
-pygame.display.gl_set_attribute(pygame.GL_MULTISAMPLESAMPLES, 4)
 
 # 2. Create an OpenGL window
-screen = pygame.display.set_mode((1080, 600), pygame.OPENGL | pygame.DOUBLEBUF)
-# 3. Now ModernGL can hook into the OpenGL context
-ctx = moderngl.create_context()
-glEnable(GL_MULTISAMPLE)
-print("ModernGL context created. Version:", ctx.version_code)
+screen = pygame.display.set_mode((1080, 600), pygame.HWSURFACE | pygame.DOUBLEBUF)
 
 # Load assets
 background = pygame.image.load("assets/images/background.png").convert()
